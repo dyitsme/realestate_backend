4b4090e6053b970fb5d9620843a8c672dab71c96
gmstan <90750887+gmstan@users.noreply.github.com>
2024-06-05 15:26:21 +0800

    xgboost feature engineering fixed

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import numpy as np
 import xgboost as xgb
 import json
 import os
+import pandas as pd
 from snn import snn_model, predict_snn
-
+from nearbyamenities import nearby_amenities
 from werkzeug.utils import secure_filename
 
 
@@ -15,12 +16,109 @@ model = xgb.XGBRegressor()
 model.load_model('xgb_model_gavin_best.json')
 
 #update when input is available
-def predict_xgb(data):
+def predict_xgb(parsed_data):
+    data = json.loads(parsed_data)
+    
+    client_data = {
+        'lat': data['coords']['lat'],
+        'lng': data['coords']['lng'],
+        'operation': data['operation'], #buy/rent
+        'saleType': data['saleType'], #new, resale
+        'furnishing': data['furnishing'], #unfurnished, semi, complete
+        'propertyType': data['propertyType'], #house, land, etc
+        'city': data['city']
+    }
+
+    df_data = {
+        'bedrooms': data['bedrooms'],
+        'bathrooms': data['bathrooms'],
+        'lotSize': data['lotSize'],
+        'floor area (m2)': data['floorArea'],
+        'build (year)': data['age'],
+        'total floors': data['totalFloors'],
+        'car spaces': data['carSpaces'],
+        "classification_Brand New": 0,
+        "classification_Resale": 0,
+        "fully furnished_No": 0,
+        "fully furnished_Semi": 0,
+        "fully furnished_Yes": 0,
+        "type_encoded": 0,
+        "operation_city_0_0": 0,
+        "operation_city_0_1": 0,
+        "operation_city_1_0": 0,
+        "operation_city_1_1": 0
+    }
+
+    # List of amenities to be added to df_data
+    amenities_list = [
+        "gym", "wi-fi", "swimming pool", "pay tv access", "basketball court", "jogging path",
+        "alarm system", "lounge", "entertainment room", "parks", "cctv", "basement parking",
+        "elevators", "fire exits", "function room", "lobby", "reception area", "fire alarm",
+        "fire sprinkler system", "24-hour security", "garden", "secure parking", "bar",
+        "maid's room", "playground", "gymnasium", "utility room", "billiards table",
+        "business center", "club house", "fitness center", "game room", "meeting rooms",
+        "multi-purpose hall", "shower rooms", "sky lounge", "smoke detector", "social hall",
+        "study room", "function area", "open space", "gazebos", "shops", "study area",
+        "carport", "clubhouse", "deck", "gazebo", "landscaped garden", "multi-purpose lawn",
+        "parking lot", "theater", "daycare center", "sauna", "laundry area", "courtyard",
+        "badminton court", "tennis court", "jacuzzi", "central air conditioning", "health club",
+        "indoor spa", "outdoor spa", "pool bar", "indoor pool", "drying area", "floorboards",
+        "split-system heating", "garage", "remote garage", "sports facilities", "powder room",
+        "maids room", "library", "spa", "clinic", "open car spaces", "intercom", "ensuite",
+        "pond", "amphitheater", "gas heating", "hydronic heating", "indoor tree house",
+        "open fireplace", "helipad", "golf area", "storage room", "terrace", "driver's room",
+        "attic", "basement", "lanai", "ducted cooling", "ducted vacuum system", "fireplace"
+    ]
+
+    # Add amenities to df_data with initial value of 0
+    for amenity in amenities_list:
+        df_data[amenity] = 0
+
+    # Update df_data based on the amenities provided in the JSON
+    for amenity in data['amenities']:
+        if amenity['isSelected']:
+            df_data[amenity] = 1
+
+    if client_data["propertyType"] == 'apartment':
+        df_data['type_encoded'] = 0
+    elif client_data['propertyType'] == 'condominium':
+        df_data['type_encoded'] = 1
+    elif client_data['propertyType'] == 'house':
+        df_data['type_encoded'] = 2
+    elif client_data['propertyType'] == 'land':
+        df_data["type_encoded"] = 3
+    else:   
+        print("INVALID")
+
+    if client_data['furnishing'] == 'unfurnished':
+        df_data['fully furnished_No'] = 1
+    elif client_data['furnishing'] == 'semi':
+        df_data['fully furnished_Semi'] = 1
+    elif client_data['furnishing'] == 'semi':
+        df_data['fully furnished_Yes'] = 1
+    else:
+        print("INVALID")
+
+    if client_data['city'] == 'pasig' and client_data['operation'] == 'buy':
+        df_data['operation_city_0_1'] = 1
+    elif client_data['city'] == 'pasig' and client_data['operation'] == 'rent':
+        df_data['operation_city_1_1'] = 1
+    elif client_data['city'] == 'paranaque' and client_data['operation'] == 'buy':
+        df_data['operation_city_0_0'] = 1
+    elif client_data['city'] == 'paranaque' and client_data['operation'] == 'rent':
+        df_data['operation_city_1_0'] = 1
+    else:
+        print("INVALID")
+        
+    main_df = pd.DataFrame([df_data])  
+
+    amenities = nearby_amenities(client_data['lng'], client_data['lat'])
+
+    final_df = pd.concat([main_df, amenities], axis=1)
+
     try:
-        # Return prediction# Reshape features for the model
-        features = np.array(data['features']).reshape(1, -1)
         # Make prediction
-        prediction = model.predict(features)
+        prediction = model.predict(final_df)
         return prediction.tolist()
     except Exception as e:
         return str(e)
@@ -30,6 +128,11 @@ def predict_xgb(data):
 def home():
     return "Model Inference API: /predict_xgb for XGBoost and /predict_snn for SNN"
 
+@app.route('/nearby-amenities', methods=['POST'])
+def nearby_endpoint():
+    nearby_amenities()
+
+    return "DONE"
 
 @app.route('/predict_xgb', methods=['POST'])
 def predict_xgb_endpoint():
---
+++ nearbyamenities.py
@@ -0,0 +1,163 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import geopandas as gpd
+import math
+import json
+from shapely.geometry import Point
+from geopy.distance import geodesic
+
+app = Flask(__name__)
+CORS(app)
+
+def nearby_amenities(longitude, latitude):
+    # Retrieve the JSON string from form data
+    json_str = request.form.get('coords')
+
+    # Parse the JSON string into a Python dictionary
+    data = json.loads(json_str)
+
+    # Access latitude and longitude values from the dictionary
+    latitude = data['lat']
+    longitude = data['lng']
+
+    print(latitude)
+    print(longitude)
+
+    point = Point(longitude, latitude)
+    print(Point)
+    # Your logic to compute nearby amenities
+    amenities = compute_nearby_amenities(point)
+
+    return amenities
+
+def read_amenities_file():
+    # Load GeoJSON file of amenities into a GeoDataFrame
+    # amenities_gdf = gpd.read_file('Amenity Files/pasig_amenities.geojson')
+    amenities_gdf = gpd.read_file('Amenity Files/pq_amenities.geojson')
+
+    amenities_gdf_projected = amenities_gdf
+
+    # Specify the distance threshold (in meters)
+    distance_threshold = 1000  # Adjust as needed
+
+def category_mapping():
+    category_mapping = {
+        'vehicle_services': {'amenities': ['fuel', 'car_wash', 'compressed_air', 'car_repair', 'car', 'car_parts', 'gas', 'battery'], 'tags': ['shop', 'amenity', 'car_parts']},
+        
+        'night_life': {'amenities': ['pub', 'bar', 'nightclub', ], 'tags': ['amenity', 'shop']},
+        
+        'personal_care': {'amenities': ['massage_chair', 'cosmetics', 'spa', 'hairdresser', 'boutique', 'optician', 'nails', 'cosmetics'], 
+                        'tags': ['shop', 'amenity', 'beauty']},
+        
+        'administrative_offices': {'amenities': ['townhall', 'police', 'fire_station', 'courthouse', 'community_centre', 'post_office', 'government', 'administrative'
+                                                ], 'tags': ['amenity', 'shop', 'office']},
+        
+        'education': {'amenities': ['school', 'college', 'kindergarten', 'prep_school', 'driving_school', 'special_needs'], 
+                    'tags': ['amenity', 'shop', 'specialized_education', 'government']},
+        
+        'food': {'amenities': ['restaurant', 'fast_food', 'cafe', 'bbq', 'food_court', 'vending_machine', 'ice_cream', 'drinking_water', 'bakery', 
+                            'pastry', 'yes', 'only'], 'tags': ['amenity', 'shop', 'delivery', 'drive_through', 'takeaway']},
+        
+        'general_establishments': {'amenities': ['toilets', 'public_building', 'post_box', 'waste_basket', 'mall', 'kiosk', 'vacant', 'yes',
+                                                'general'], 'tags': ['amenity', 'shop']},
+        
+        'healthcare_industries': {'amenities': ['pharmacy', 'hospital', 'veterinary', 'doctors', 'clinic', 'dentist', 'health_post', 'childcare', 'blood_check', 'pedia', 
+                                                'community_health_worker'], 'tags': ['amenity', 'shop', 'healthcare:speciality', 'healthcare']},
+        
+        'service_providers': {'amenities': ['car_sharing', 'internet_cafe', 'telephone', 'post_depot', 'car_rental', 'payment_terminal', 'loading_dock', 
+                                            'payment_centre', 'crematorium', 'events_venue', 'shelf', 'massage', 'laundry', 'butcher', 'ticket', 'travel_agency',
+                                        'tailor', 'trade', 'printing', 'copyshop', 'pet_grooming', 'funeral_directors', 'telecommunication', 'wlan', 'terminal', 
+                                        'service', 'yes'], 'tags': ['amenity', 'shop', 'internet_access']},
+        
+        'recreational': {'amenities': ['bench', 'cinema', 'fountain', 'training', 'smoking_area', 'casino', 'library'], 'tags': ['amenity', 'shop']},
+        
+        'living_facilities': {'amenities': ['shelter', 'lounge'], 'tags': ['amenity', 'shop']},
+        
+        'religion': {'amenities': ['place_of_worship', 'monastery'], 'tags': ['amenity', 'shop']},
+        
+        'financial': {'amenities': ['bank', 'bureau_de_change', 'atm', 'money_transfer', 'money_lender'], 'tags': ['amenity', 'shop']},
+        
+        'specialized_stores': {'amenities': ['convenience', 'marketplace', 'public_market', 'supermarket', 'books', 'electronics', 'fabric', 
+                                            'art', 'garden_centre', 'computer', 'health', 'water', 'baby_goods', 'furniture', 'water_station', 
+                                            'doityourself', 'hardware', 'advertising', 'guns', 'pet', 'motorcycle', 'department_store', 'bicycle',
+                                            'seafood', 'greengrocer', 'tyres', 'pawnbroker', 'video_games', 'hifi', 'shoes', 'tea', 'clothes',
+                                            'stationery', 'wine;alcohol', 'beauty', 'appliance', 'e-cigarette', 'confectionery', 'alcohol', 'sports',
+                                            'beverages', 'variety_store', 'coffee', 'medical_supply', 'chemist', 'party', 'health_food',
+                                            'paint', 'gift', 'jewelry', 'bag', 'perfumery', 'bed', 'watches', 'rice', 'electrical', 'florist', 'photo',
+                                            'houseware', 'fashion_accessories', 'frozen_food', 'interior_decoration', 'model', 'farm'], 'tags': ['amenity', 'shop']},
+        
+        'transportation': {'amenities': ['taxi', 'parking', 'parking_entrance', 'bus_station', 'motorcycle_parking', 'bicycle_parking', 
+                                        'parking_space', 'subway_entrance'], 'tags': ['amenity', 'shop', 'railway']},
+        # Add more categories and corresponding amenities/tags as needed
+    }
+    
+
+def walkability_score(distance):
+    
+    if(distance > 1000):
+        return 0
+    
+    e = math.e
+    
+    walk_score = e**(-5*(distance/1000)**5)
+    return walk_score
+
+def compute_nearby_amenities(point):
+    read_amenities_file()
+    category_mapping()
+    walkability_score()
+
+    amenities_data = pd.DataFrame()
+
+    # Iterate over each listing
+    listing_shortest_distances = {category: float('inf') for category in category_mapping}
+    listing_distance_sum = {category: 0 for category in category_mapping}
+    listing_num = {category: 0 for category in category_mapping}
+        
+        for amenity_idx, amenity_row in amenities_gdf_projected.iterrows():
+            # print("LISTING",listing.geometry)
+            # print("AMENITY",amenity_row.geometry)
+            # print(amenity_row.amenity)
+            
+            # amen_distance = get_route_coordinates((listing.geometry.y,listing.geometry.x),(amenity_row.geometry.y,amenity_row.geometry.x))
+            # print(amen_distance)   
+            
+            distance = geodesic((point.geometry.y, point.geometry.x), (amenity_row.geometry.y, amenity_row.geometry.x)).meters
+            # print("Distance using geodesic:", distance_geodesic)
+            
+
+            # Check if the amenity falls under any category
+            for category, details in category_mapping.items():
+                
+                column_name1 = category + "_nearest_distance"
+                column_name2 = category + "_walkability_score"
+                column_name3 = category + "_avg_distance"
+                column_name = category
+
+                if amenity_row.amenity in details['amenities']:
+                    if (distance <= distance_threshold):
+                        
+                        listing_distance_sum[category] += distance
+                        listing_num[category] += 1
+                        amenities_data.at[0, column_name3] = listing_distance_sum[category] / listing_num[category]
+                        amenities_data.at[0, column_name] = listing_num[category]
+                        
+                        # Update shortest distance if the current distance is shorter
+                        if (listing_shortest_distances[category] is None or distance < listing_shortest_distances[category]):
+                            listing_shortest_distances[category] = distance
+                            amenities_data.at[0, column_name1] = distance
+                            amenities_data.at[0, column_name2] = walkability_score(distance)
+                                                                
+                    break  # No need to continue checking other categories for this amenity
+    
+        for category, distance in amenities_data.items():
+            print(f"Shortest distance to {category}: {distance}")
+
+    return amenities_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
